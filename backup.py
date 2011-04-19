e83cd5c677d0e3e683c6f9dfba0bd0a0a2d0a4ef
Robby Ranshous <rranshous@gmail.com>
2011-04-19 00:34:20 -0400

    now deleting from server local deletes

--- backup.py
+++ backup.py
@@ -6,11 +6,11 @@ DATA_DIR = './data'
 # the goal for now is to simply upload the data via sftp
 import paramiko
 from findfiles import find_files_iter as find_files
-import sys, os
+import sys, os, random
+import json
+
 
 # get the username and password
-print 'args: %s' % sys.argv
-
 assert len(sys.argv) >= 4, 'must specify host, username, password'
 
 host,username,password = tuple(sys.argv[1:4])
@@ -42,10 +42,13 @@ DATA_DIR = os.path.abspath(DATA_DIR)
 
 print 'uploading from: %s' % DATA_DIR
 
+# collect up a snapshot of our state
+snapshot = {}
+
 # go through all the files recursively
 for file_path in find_files(DATA_DIR):
 
-    # we are going to upload every file blindly
+    # where are we uploading it to ?
     rel_path = file_path[len(DATA_DIR)+1:]
 
     print 'uploading rel_path: %s' % rel_path
@@ -53,6 +56,32 @@ for file_path in find_files(DATA_DIR):
     # this has a callback function .. interesting
     sftp.put(file_path,rel_path)
 
+    # add this to our snapshot
+    snapshot[rel_path] = {}
+
     print 'done uploading'
 
+print 'reading old snapshot'
+
+# read in the old snapshot
+if os.path.exists('snapshot.json'):
+    with file('./snapshot.json','r') as fh:
+        old_snapshot = json.loads(fh.read())
+else:
+    print 'old snapshot not found'
+    old_snapshot = {}
+
+# we want to delete anything from the server
+# which was in our snapshot but isn't any longer
+diff = set(old_snapshot.keys()) - set(snapshot.keys())
+for rel_path in diff:
+    print 'deleting from server: %s' % rel_path
+    sftp.remove(rel_path)
+
+# save out our snapshot
+print 'writing new snapshot'
+with file('snapshot.json','w') as fh:
+    fh.write(json.dumps(snapshot))
+
+print
 print 'done'
